d790064d8420dec799b930629653a822a93a9822
Sahil <sahilchandwani347@gmail.com>
2024-08-01 15:38:28 +0530

    hello

--- components/language.js
+++ components/language.js
@@ -2,28 +2,70 @@ import React, { useRef, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 
 function Language(props) {
-  let value = props.value
-  let setValue = props.setValue
-  let list = useRef()
+  let value = props.value;
+  let setValue = props.setValue;
+  let list = useRef();
   return (
     <div className="py-3 inline-flex justify-center flex-col">
       <div className="flex items-center">
-      <div className="px-4 text-xl text-white">Language: </div>
-      <div className="text-xl px-3 text-white bg-slate-600 h-12 flex items-center justify-center shadow-lg rounded-lg">
-        <div className="mx-2">{value}</div>
-        <IoMdArrowDropdown className="text-3xl" onClick={()=>{
-          if(list.current.style.display == "" || list.current.style.display == "none"){
-          list.current.style.display = "block"
-        }else{
-          list.current.style.display = "none"
-        }}} />
+        <div className="px-4 text-xl text-white cursor-default">Language: </div>
+        <div
+          className="text-xl px-3 cursor-pointer text-white bg-slate-600 h-12 flex items-center justify-center shadow-lg rounded-lg"
+          onClick={() => {
+            if (
+              list.current.style.display == "" ||
+              list.current.style.display == "none"
+            ) {
+              list.current.style.display = "block";
+            } else {
+              list.current.style.display = "none";
+            }
+          }}
+        >
+          <div className="mx-2">{value}</div>
+          <IoMdArrowDropdown className="text-3xl" />
+        </div>
       </div>
-      </div>
-      <div ref={list} className="hidden absolute left-[7.6rem] top-16 z-10 bg-slate-600 w-40 rounded-lg shadow-lg">
-        <div className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg" onClick={()=>{setValue("javascript")}}>javaScript</div>
-        <div className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg" onClick={()=>{setValue("python")}}>Python</div>
-        <div className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg" onClick={()=>{setValue("c++")}}>C++</div>
-        <div className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg" onClick={()=>{setValue("csharp")}}>csharp</div>
+      <div
+        ref={list}
+        className="hidden absolute left-[8.5rem] cursor-pointer top-32 z-10 bg-slate-600 w-40 rounded-lg shadow-lg"
+      >
+        <div
+          className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg"
+          onClick={() => {
+            setValue("javascript");
+            list.current.style.display = "none";
+          }}
+        >
+          javaScript
+        </div>
+        <div
+          className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg"
+          onClick={() => {
+            setValue("python");
+            list.current.style.display = "none";
+          }}
+        >
+          Python
+        </div>
+        <div
+          className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg"
+          onClick={() => {
+            setValue("c++");
+            list.current.style.display = "none";
+          }}
+        >
+          C++
+        </div>
+        <div
+          className="text-white py-2 px-4 hover:bg-slate-900 rounded-lg"
+          onClick={() => {
+            setValue("csharp");
+            list.current.style.display = "none";
+          }}
+        >
+          csharp
+        </div>
       </div>
     </div>
   );
